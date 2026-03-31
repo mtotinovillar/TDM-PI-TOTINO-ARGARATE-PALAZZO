77571a5596bc539ef69d90cfc9e0a6b30c480a60
mtotinovillar <mtotinovillar@udesa.edu.ar>
2026-03-31 17:08:38 -0300

    boton ver mas en card

--- pi/src/components/Card/Card.js
+++ pi/src/components/Card/Card.js
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 
-function Card(props) {
-  return (
-    <article className="card">
-      <img className="imagen"
-        src={"https://image.tmdb.org/t/p/w342/" + props.imagen}  alt={props.titulo} />
+class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      verMas: false
+    };
+  }
 
-  <h2 className="titulo">{props.titulo}</h2>
+  mostrarInfo() {
+    this.setState({
+      verMas: !this.state.verMas
+    });
+  }
 
-      <p>{props.descripcion}</p>
+  render() {
+    return (
+      <article className="card">
+        <img
+          className="imagen"
+          src={"https://image.tmdb.org/t/p/w342/" + this.props.imagen} alt={this.props.titulo}
+        />
 
-    
+        <h2 className="titulo">{this.props.titulo}</h2>
 
-    </article>
+        {this.state.verMas ? <p>{this.props.descripcion}</p> : null}
 
-  );
+        <button className="more" onClick={() => this.mostrarInfo}>
+          {this.state.verMas ? "Ver menos" : "Ver más"}
+        </button>
+      </article>
+    );
+  }
 }
 
 export default Card;
@@ -22,7 +39,6 @@ export default Card;
 
 /*
 falta: 
-Link o botón "ver descripción" que debe mostrar/ ocultar la descripción.
 Link o botón “ir a detalle” para navegar hasta la página de detalle del elemento.
 Link, botón o ícono "agregar / quitar de favoritos", solamente disponible si la cookie de sesión existe.
 */
