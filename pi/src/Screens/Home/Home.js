6409ccbce434c0de18ff6bb03fe57becbe0fe6d3
mtotinovillar <mtotinovillar@udesa.edu.ar>
2026-03-31 16:50:14 -0300

    card

---
+++ pi/src/Screens/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Peliculas from "../../components/Peliculas/Peliculas";
+
+function Home() {
+  return (
+    <>
+      <Peliculas />
+    </>
+  );
+}
+
+export default Home;
--- pi/src/components/Card/Card.js
+++ pi/src/components/Card/Card.js
@@ -1,1 +1,28 @@
 import React from "react";
+
+function Card(props) {
+  return (
+    <article className="card">
+      <img className="imagen"
+        src={"https://image.tmdb.org/t/p/w342/" + props.imagen}  alt={props.titulo} />
+
+  <h2 className="titulo">{props.titulo}</h2>
+
+      <p>{props.descripcion}</p>
+
+    
+
+    </article>
+
+  );
+}
+
+export default Card;
+
+
+/*
+falta: 
+Link o botón "ver descripción" que debe mostrar/ ocultar la descripción.
+Link o botón “ir a detalle” para navegar hasta la página de detalle del elemento.
+Link, botón o ícono "agregar / quitar de favoritos", solamente disponible si la cookie de sesión existe.
+*/
