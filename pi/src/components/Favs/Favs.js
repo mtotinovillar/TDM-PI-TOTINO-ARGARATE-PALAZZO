3d82a54a3a2d8cff67c5dd2c358b5fee9e91c45b
morena-lab <morenapalazzo@gmail.edu.ar>
2026-04-16 21:17:09 -0300

    cookies en Favs

--- pi/src/components/Favs/Favs.js
+++ pi/src/components/Favs/Favs.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
-import "../Favs/favs.css"
+import "../Favs/favs.css";
+import Cookies from "universal-cookie";
+
+const cookies = new Cookies();
 
 class Favs extends Component {
   constructor(props) {
@@ -9,55 +12,45 @@ class Favs extends Component {
     };
   }
 
-  componentDidMount() {
-  let clave = this.props.tipo === "movie" ? "favPelis" : "favSeries";
-  let storage = localStorage.getItem(clave);
-  let storageParseado = JSON.parse(storage);
-
-  if (storageParseado !== null) {
-    let existe = storageParseado.filter(elemento => elemento == (this.props.id));
-
-    if (existe.length > 0) {
-      this.setState({
-        esFav: true
-      });
-    }
-  }
-}
-
   agregarFav(id) {
-    let clave = this.props.tipo === "movie" ? "favPelis" : "favSeries";
-    let storage = localStorage.getItem(clave);
+
+    
+    let storage = localStorage.getItem("favPelis");
     let storageParseado = JSON.parse(storage);
 
     if (storageParseado === null) {
       let primerValor = [id];
       let primerValorString = JSON.stringify(primerValor);
-      localStorage.setItem(clave, primerValorString);
+      localStorage.setItem("favPelis", primerValorString);
     } else {
       storageParseado.push(id);
       let storageString = JSON.stringify(storageParseado);
-      localStorage.setItem(clave, storageString);
+      localStorage.setItem("favPelis", storageString);
     }
 
     this.setState({ esFav: true });
   }
 
   sacarFav(id) {
-    let clave = this.props.tipo === "movie" ? "favPelis" : "favSeries";
-    let storage = localStorage.getItem(clave);
+    let storage = localStorage.getItem("favPelis");
     let storageParseado = JSON.parse(storage);
 
     if (storageParseado !== null) {
       let storageFiltrado = storageParseado.filter(elemento => elemento !== id);
       let storageString = JSON.stringify(storageFiltrado);
-      localStorage.setItem(clave, storageString);
+      localStorage.setItem("favPelis", storageString);
     }
 
     this.setState({ esFav: false });
   }
 
   render() {
+    let sesion = cookies.get("user-auth-cookie");
+
+    if (!sesion){
+      return null;
+    }
+    
     return (
         <button
           className={this.state.esFav ? "favorito activo" : "favorito"}
@@ -69,6 +62,7 @@ class Favs extends Component {
         >
           ♥
         </button>
+      
     );
   }
 }
