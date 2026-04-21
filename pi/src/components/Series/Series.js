c0a6bd508dc4665a4db5e052ccc0788d97f5bcff
valenargarate <vargaratemena@udesa.edu.ar>
2026-04-20 21:47:40 -0300

    arreglo en series

--- pi/src/components/Series/Series.js
+++ pi/src/components/Series/Series.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Card from "../Card/Card";
-import "./Series.css";
+import "./series.css";
 import { Link } from "react-router-dom";
 
 class Series extends Component {
