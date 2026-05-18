c1c027a576252a6b2f417e4576af3d52f4474e05
valenargarate <vargaratemena@udesa.edu.ar>
2026-05-18 15:06:10 -0300

    termine register con hooks

--- pi/src/Screens/Register/Register.js
+++ pi/src/Screens/Register/Register.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./register.css"
-class Register extends Component {
+
+/* class Register extends Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -116,24 +117,24 @@ class Register extends Component {
     }
 }
 
-export default Register
+export default Register */
 
 function Register(props) {
-    const [userName, setUser] = useState(""),
-    const [email, setEmail] = useState (""),
-    const [password, setPassword] = useState(""),
-    const [error, setError] = useState("")
+    const [userName, setUser] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
 
-    controlarCambios = (event) => {
+    let controlarCambios = (event) => {
         if (event.target.name === "email") {
             setEmail(event.target.value)
-        },
+        } return;
         if (event.target.name === "password") {
             setPassword(event.target.value)
-        }
+        } return;
     }
 
-    submit = (event) => {
+    let submit = (event) => {
         event.preventDefault();
 
         let user = {
@@ -142,77 +143,79 @@ function Register(props) {
         }
         if (password.length < 6) {
             setError("La contraseña tener mínimo 6 caracteres")
-        }
+        } return;
 
-        let userStorage = localStorage.getItem("users");
+        let usersStorage = localStorage.getItem("users");
 
-        if (userStorage !== null) {
+        if (usersStorage !== null) {
 
             let usersParseado = JSON.parse(usersStorage);
             let usersFiltrado = usersParseado.filter(user => user.email === email);
 
             if (usersFiltrado.length > 0) {
                 setError("Ya existe un usuario con el mail ingresado")
-            }
+            } return;
             usersParseado.push(user);
-            let usersEnJson = JSON.stringify(userParseado)
+            let usersEnJson = JSON.stringify(usersParseado)
             localStorage.setItem("users", usersEnJson)
-        } else{
+        } else {
             let usersInicial = [user];
             let usersEnJson = JSON.stringify(usersInicial)
             localStorage.setItem("users", usersEnJson)
         }
 
-        // COMO ENTRA HISTORY
+        props.history.push("/login");
 
     }
 
     return (
+        <div>
             <div>
-                <div>
-                    <h2 className="alert alert-primary">Registrarse</h2>
-                </div>
-                <div className="row justify-content-center">
-                    <div className="col-md-6">
-                        <form onSubmit={submit} >
-                            <div className="form-group">
-                                <label className="email">Email</label>
+                <h2 className="alert alert-primary">Registrarse</h2>
+            </div>
+            <div className="row justify-content-center">
+                <div className="col-md-6">
+                    <form onSubmit={submit} >
+                        <div className="form-group">
+                            <label className="email">Email</label>
 
-                                <input
-                                    className="form-control"
-                                    type="text"
-                                    name="email"
-                                    placeholder="Ingresá tu email"
-                                    value={email}
-                                    onChange={controlarCambios}
-                                />
-                            </div>
+                            <input
+                                className="form-control"
+                                type="text"
+                                name="email"
+                                placeholder="Ingresá tu email"
+                                value={email}
+                                onChange={controlarCambios}
+                            />
+                        </div>
 
-                            <div className="form-group">
-                                <label className="password">Contraseña</label>
-                                <input
-                                    className="form-control"
-                                    type="password"
-                                    name="password"
-                                    placeholder="Ingresá tu contraseña"
-                                    value={password}
-                                    onChange={controlarCambios}
-                                />
-                            </div>
-                            <button className="btn btn-primary btn-block" type="submit">Registrarse</button>
+                        <div className="form-group">
+                            <label className="password">Contraseña</label>
+                            <input
+                                className="form-control"
+                                type="password"
+                                name="password"
+                                placeholder="Ingresá tu contraseña"
+                                value={password}
+                                onChange={controlarCambios}
+                            />
+                        </div>
+                        <button className="btn btn-primary btn-block" type="submit">Registrarse</button>
 
-                            {error !== "" ?
-                                <p>{error}</p>
-                                :
-                                null
-                            }
-                        </form>
-                        <p className="mt-3 text-center">¿Ya tenés cuenta?
-                            <Link to="/login" className="link">Iniciar sesión</Link>
-                        </p>
-                    </div>
+                        {error !== "" ?
+                            <p>{error}</p>
+                            :
+                            null
+                        }
+                    </form>
+                    <p className="mt-3 text-center">¿Ya tenés cuenta?
+                        <Link to="/login" className="link">Iniciar sesión</Link>
+                    </p>
                 </div>
             </div>
+        </div>
 
-        );
+    );
 }
+
+export default Register;
